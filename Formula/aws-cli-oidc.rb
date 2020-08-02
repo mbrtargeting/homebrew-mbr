c458aeb9a3ce65d93732965d3445b2847ee98947
Ilya Verbitskiy <ilya@verbit.io>
2020-08-02 18:08:47 +0200

    aws-cli-oidc: shorten description for test-bot

--- Formula/aws-cli-oidc.rb
+++ Formula/aws-cli-oidc.rb
@@ -1,5 +1,5 @@
 class AwsCliOidc < Formula
-  desc "CLI tool for retrieving AWS temporary credentials using OIDC provider"
+  desc "Tool for retrieving AWS temporary credentials using OIDC provider"
   homepage "https://github.com/mbrtargeting/aws-cli-oidc"
   url "https://github.com/mbrtargeting/aws-cli-oidc/archive/rel-95.tar.gz"
   sha256 "59aed7ea14863c2d012a59b09321bc1e1692bb032c1a15e44aa3bb9c0f7e0b89"
